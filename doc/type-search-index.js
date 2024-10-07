33edef66608fb8a2ca7447e01661f069d0357b10
Dibya Jyoti Goswami <dibyaj.gos@gmail.com>
2024-10-06 22:18:37 -0400

    Javadoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"lab3","l":"ComplexNumber"},{"p":"lab3","l":"RationalNumber"}];updateSearchResults();
--- lab3/Lab3Exception.java
+++ lab3/Lab3Exception.java
@@ -1,6 +1,6 @@
 package lab3;
 
-class Lab3Exception extends Exception{
+public class Lab3Exception extends Exception{
     public String message;
 
     public Lab3Exception(String input){
